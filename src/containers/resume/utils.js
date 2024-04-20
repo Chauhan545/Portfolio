6e7bbc77a0eb9de9a2c74f52367cab4c2245cc8e
Chauhan545 <shivanichauh0791@gmail.com>
2024-04-20 21:44:16 +0530

    first

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,42 @@
+export const data = {
+    experience: [
+      {
+        title: "Online Grocery Store",
+        subTitle: "Apna Farm",
+        description:
+          " Using HTML, CSS, JS, Bootstrap",
+      },
+      {
+        title: "Sudoku Solver",
+        subTitle: "Using the concept of Recursion and Backtracking",
+        description:
+          "puzzle game ",
+      },
+      {
+        title: "French Learning Website",
+        subTitle: "Using HTML, CSS,JS",
+        description:
+          "Make learning french easy and enjoyable!",
+      },
+    ],
+    education: [
+      {
+        title: "Secondary",
+        subTitle: "Alpine Public School",
+        description:
+          "10th Grade",
+      },
+      {
+        title: "Higher Secondary",
+        subTitle: "Alpine Public School",
+        description:
+          "12th Grade in Science",
+      },
+      {
+        title: "B.Tech",
+        subTitle: "Lovely Professional University Phagwara Punjab",
+        description:
+          "Bachelor of technology in computer science",
+      },
+    ],
+  };
